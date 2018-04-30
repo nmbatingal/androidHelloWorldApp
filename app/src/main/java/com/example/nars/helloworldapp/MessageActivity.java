62dc6a3c7da5b9606d71b1accfe0355c634c1b67
Narciso Batingal <batingalnarz11@gmail.com>
2018-04-30 12:07:00 +0800

    added final activity

--- app/src/main/java/com/example/nars/helloworldapp/MessageActivity.java
+++ app/src/main/java/com/example/nars/helloworldapp/MessageActivity.java
@@ -1,7 +1,9 @@
 package com.example.nars.helloworldapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
@@ -11,10 +13,15 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        // String message = getIntent().getExtras().getString("mainactivity");
-        String textMessage = getIntent().getExtras().getString("com.example.nars.USER_MESSAGE");
-        TextView textUserMessage = (TextView) findViewById(R.id.textUserMessage);
+        // Intent intent = getIntent();
+        // String textMessage = intent.getStringExtra("com.example.nars.USER_MESSAGE");
+        String textMessage = getIntent().getStringExtra("com.example.nars.USER_MESSAGE");
+        TextView textUserMessage = (TextView) findViewById(R.id.textViewMessage);
 
         textUserMessage.setText(textMessage);
+    }
+
+    public void openFinalActivity(View view) {
+        startActivity(new Intent(this, FinalActivity.class));
     }
 }
